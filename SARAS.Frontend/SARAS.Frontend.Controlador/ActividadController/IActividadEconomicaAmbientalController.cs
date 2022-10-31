7af213a204cf89a716fea4905458e2cba94c3967
KislevBecker <kisbecker@gmail.com>
2022-10-31 08:27:21 -0600

    agregado lo de actividad ambiental

---
+++ SARAS.Frontend/SARAS.Frontend.Controlador/ActividadController/IActividadEconomicaAmbientalController.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+namespace SARAS.Frontend.Controlador.ActividadController
+{
+    public interface IActividadEconomicaAmbientalController 
+    {
+        public string[] Wizard { get; set; }
+        public bool MostrarLoader { get; set; }
+        public string BarraPorcentaje { get; set; }
+        public bool MostrarBotonesWizard { get; set; }
+        void AtrasStep();
+        Task Siguiente();
+    }
+}
--- SARAS.Frontend/SARAS.Frontend.Wasm/Program.cs
+++ SARAS.Frontend/SARAS.Frontend.Wasm/Program.cs
@@ -1,11 +1,41 @@
+using Blazored.Modal;
+using Blazored.SessionStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using SARAS.Frontend.FuenteDatos.HttpRepositorio;
+using SARAS.Frontend.PD.GenericaPersistenciaDatos;
+using SARAS.Frontend.Transversal.Helpers;
 using SARAS.Frontend.Wasm;
+
+//using Blazored.Modal;
+//using Blazored.SessionStorage;
+//using SARASWEB.Frontend.Aplicacion.ListasGeneralesAplicacion.BancosAplicacion;
+//using SARASWEB.Frontend.Aplicacion.ListasGeneralesAplicacion.MonedasAplicacion;
+//using SARASWEB.Frontend.Aplicacion.ListasGeneralesAplicacion.TipoCuentasAplicacion;
+//using SARASWEB.Frontend.Aplicacion.ListasGeneralesAplicacion.TipoDocumentoAplicacion;
+//using SARASWEB.Frontend.Aplicacion.ProductosAplicacion;
+//using SARASWEB.Frontend.Controlador.TransferenciaPropiaControlador;
+//using SARASWEB.Frontend.Controlador.TransferenciaTercerosControlador;
+//using SARASWEB.Frontend.FuenteDatos.HttpRepositorio;
+//using SARASWEB.Frontend.PD.GenericaPersistenciaDatos;
+//using SARASWEB.Frontend.PD;
+//using SARASWEB.Frontend.Transversal.Helpers;
+//using SARASWEB.Frontend.Wasm;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddTransient<IHelpers, Helpers>();
+builder.Services.AddTransient<IHttpRepositorio, HttpRepositorio>();
+builder.Services.AddTransient<IGenericaPersistenciaDatos, GenericaPersistenciaDatos>();
+builder.Services.AddBlazoredModal();
+builder.Services.AddBlazoredSessionStorage();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddBlazoredSessionStorage(config =>
+config.JsonSerializerOptions.WriteIndented = true);
+
 
 await builder.Build().RunAsync();
